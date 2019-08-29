47cb5a39b4b40309cc17d006f4089adaae0d194e
Konrad <konradsusel@gmail.com>
2019-08-29 14:38:01 +0200

    First connection with googlesheets

--- src/main/java/code/App.java
+++ src/main/java/code/App.java
@@ -3,12 +3,43 @@ package code;
 import code.Controller.SQLController;
 import code.view.View;
 import code.dao.Dao;
-
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.*;
 import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.List;
 
-public class App
-{
+public class App {
+
+    private static Sheets sheetsService;
+    private static String SPREADSHEET_ID = "1ZLJ7lJo0RZcafF1ha6FG4J6mSXEAglVfNfIngt4LRvM";
+
     public static void main( String[] args ) {
+
+        SheetsServiceUtil sheetsServiceUtil = new SheetsServiceUtil();
+        try {
+            sheetsService = SheetsServiceUtil.getSheetsService();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+
+        List<String> ranges = Arrays.asList("B2","B5");
+        try {
+            BatchGetValuesResponse readResult = sheetsService.spreadsheets().values()
+                    .batchGet(SPREADSHEET_ID)
+                    .setRanges(ranges)
+                    .execute();
+            ValueRange costam = readResult.getValueRanges().;
+            System.out.println(costam);
+            System.out.println(costam.getValues());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         Dao dao = new Dao(new File("src/main/resources/employees.csv"));
         View view = new View();
@@ -16,6 +47,5 @@ public class App
 
         view.typeQuery();
         sqlController.validateQuery(view.getInputFromUser());
-
     }
 }
---
+++ src/main/java/code/GoogleAuthorizeUtil.java
@@ -0,0 +1,36 @@
+package code;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.MemoryDataStoreFactory;
+import com.google.api.services.sheets.v4.SheetsScopes;
+
+public class GoogleAuthorizeUtil {
+    public static Credential authorize() throws IOException, GeneralSecurityException {
+
+        // build GoogleClientSecrets from JSON file
+        InputStream in = GoogleAuthorizeUtil.class.getResourceAsStream("/credentials.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JacksonFactory.getDefaultInstance(), new InputStreamReader(in));
+
+        List<String> scopes = Arrays.asList(SheetsScopes.SPREADSHEETS);
+
+        // build Credential object
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), clientSecrets, scopes).setDataStoreFactory(new MemoryDataStoreFactory())
+                .setAccessType("offline").build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+
+        return credential;
+    }
+}
